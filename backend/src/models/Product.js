a0cef6b682fe563b1439294b9608976d942e391b
Edwin Ortiz <edarcode@gmail.com>
2022-01-16 20:43:17 -0500

    (backend/src/models/Product.js)seteando modelo Product

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -4,9 +4,55 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("Product", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    purchasePrice: {
+      type: DataTypes.REAL,
+      allowNull: false,
+      validate: {
+        min: 0,
+      },
+    },
+    salePrice: {
+      type: DataTypes.REAL,
+      allowNull: false,
+      validate: {
+        min: 0,
+      },
+    },
+    mainImg: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 0,
+      },
+    },
+    discount: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 100,
+      },
     },
   });
 };
