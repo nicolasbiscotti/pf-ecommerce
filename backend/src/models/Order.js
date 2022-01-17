60930fe7399e5bba0e86e176992caabebf8c6057
Edwin Ortiz <edarcode@gmail.com>
2022-01-16 20:53:54 -0500

    (backend/src/models/*) creando modelos

---
+++ backend/src/models/Order.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "Order",
+    {
+      date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM("pending", "done"),
+        defaultValue: "pending",
+      },
+      adress: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
