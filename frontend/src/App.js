ba505352417417c6f69ef76045c7af350f6d282b
FerrnA <arriondovfernando@gmail.com>
2022-01-17 21:26:32 -0300

    feature(frontend/src/App.js): routeo del frontend: solo NavBar. Componentes para agregar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,32 @@
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
 import { AppStyled } from "./AppStyle";
+
+import NavBar from './components/NavBar/index';
+import TestforNavroutes from './components/TestforNavroutes';
+// ------------Añadir componentes: --------------//
+//import Shop? from './components/Shop/index';                             -Home Shop
+//import ProductDetail? from './components/ProductDetail/index';           -Detalle del producto
+//import ProductCreateForm? from './components/ProductCreateForm/index';   -Form creación de producto
+
 
 function App() {
   return (
     <AppStyled className="App">
-      <h1>pf-eccommerce</h1>
+      <Routes>
+        {/* 
+        <Route path="/" element={<LandingPage/>}/>  ---------landing Page---------  */}
+
+        <Route path="shop/*" element={<NavBar/>}>
+          {/*-Rutas de los componentes:  1.HomeSHOP(Shop)  2.DETALLEPROD(ProductDetail)  3.FORMCREACPROD(ProductCreateForm)
+              <Route path="" element={<Shop?/>}/>
+              <Route path={`/shop/products/:idProduct`} element={<ProductDetail/>} />
+              <Route path="?product/addproduct" element={<ProductCreateForm/>}/> */
+          }
+          <Route path="asd" element={<TestforNavroutes/>}/>
+        </Route>
+        
+      </Routes>
     </AppStyled>
   );
 }
