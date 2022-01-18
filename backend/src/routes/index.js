15bbb6791d1e41a3f558e79c959a247e5fc0ddba
Edwin Ortiz <edarcode@gmail.com>
2022-01-18 06:54:40 -0500

    refactor (modified:   src/routes/index.js)

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,14 +1,9 @@
 const { Router } = require("express");
 const products = require("./products/get/products");
-const suppliers = require("./suppliers/post/suppliers")
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
+const suppliers = require("./suppliers/post/suppliers");
+module.exports = router;
 
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-
 router.use("/products", products);
 router.use("/suppliers", suppliers);
-module.exports = router;
