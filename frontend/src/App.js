1bcfe518aa4c80e7fc7d3f5445620ec4cc02e5f1
FerrnA <arriondovfernando@gmail.com>
2022-01-18 17:38:07 -0300

    refactor(frontend/App): cambio de la rutas refat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,25 @@
-import React from 'react';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { AppStyled } from "./AppStyle";
 
-import NavBar from './components/NavBar/index';
+import NavBar from "./components/NavBar/index";
 // ------------Añadir componentes: --------------//
-//import Shop? from './components/Shop/index';                             -Home Shop
-//import ProductDetail? from './components/ProductDetail/index';           -Detalle del producto
-//import ProductCreateForm? from './components/ProductCreateForm/index';   -Form creación de producto
-
+//import Shop from './components/Shop/index';                             //-Home Shop
+//import ProductDetail from './components/ProductDetail/index';           //-Detalle del producto
+//import ProductCreateForm from './components/ProductCreateForm/index';   //-Form creación de producto
 
 function App() {
   return (
     <AppStyled className="App">
+      <NavBar />
       <Routes>
-        {/* 
-        <Route path="/" element={<LandingPage/>}/>  ---------landing Page---------  */}
-
-        <Route path="shop/*" element={<NavBar/>}>
-          {/*-Rutas de los componentes:  1.HomeSHOP(Shop)  2.DETALLEPROD(ProductDetail)  3.FORMCREACPROD(ProductCreateForm)
-              <Route path="" element={<Shop?/>}/>
-              <Route path={`/shop/products/:idProduct`} element={<ProductDetail/>} />
-              <Route path="?product/addproduct" element={<ProductCreateForm/>}/> */
-          }
+        <Route path="shop" />
+        <Route path="favorites" />
+        <Route path="/home" />
+        <Route path={`/details/:idProduct`} />
+        <Route path="dashboard/*">
+          <Route path="addproducts" />
         </Route>
-        
       </Routes>
     </AppStyled>
   );
