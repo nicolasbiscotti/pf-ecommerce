8baf60bfc60755938e368a83e6dcafc7b4879987
Edwin Ortiz <edarcode@gmail.com>
2022-01-18 14:48:20 -0500

    refactor(backend/src/routes/products/controllerGet/getProduct.controller.js)

--- backend/src/routes/products/products.router.js
+++ backend/src/routes/products/products.router.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
-const getProdcut = require("./controllerGet/getProduct.controller");
+const getProduct = require("./controllerGet/getProduct.controller");
 const productById = require("./controllerGet/productById.controller");
 const createProduct = require("./controllerPost/createProduct.controller");
 
 const products = Router();
 
 products.route("/id/:id").get(productById);
-products.route("/").get(getProdcut);
+products.route("/").get(getProduct);
 products.route("/").post(createProduct);
 
 module.exports = products;
