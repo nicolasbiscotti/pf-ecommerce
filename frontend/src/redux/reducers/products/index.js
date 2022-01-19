ca11710c5f32e0d9ca69f7e423c9b0b2619611f1
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-01-19 15:55:58 -0300

    refactor(frontend/../reducers/products/index.js): rename store to state

--- frontend/src/redux/reducers/products/index.js
+++ frontend/src/redux/reducers/products/index.js
@@ -1,27 +1,27 @@
 import { LOADING, SET_PRODUCT_DETAIL } from "./actions";
 
-const initialStore = {
+const initialState = {
   isLoading: false,
   product: {},
 };
 
-export const products = (store = initialStore, { type, payload }) => {
+export const products = (state = initialState, { type, payload }) => {
   switch (type) {
     /* case value:
       return; */
     case LOADING:
       return {
-        ...store,
+        ...state,
         isLoading: true,
       };
     case SET_PRODUCT_DETAIL:
       return {
-        ...store,
+        ...state,
         product: payload,
         isLoading: false,
       };
 
     default:
-      return store;
+      return state;
   }
 };
