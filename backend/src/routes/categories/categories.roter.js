86e60c89057016ae6522b668806546371e78bb2e
Edwin Ortiz <edarcode@gmail.com>
2022-01-19 06:36:15 -0500

    feat(backend/src/routes/categories) get all categories

--- backend/src/routes/categories/categories.roter.js
+++ backend/src/routes/categories/categories.roter.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const categories = Router();
+const getCategories = require("./controllerGet/getCategories");
 const createCategories = require("./controllerPost/createCategory.controller");
 
 categories.route("/").post(createCategories);
+categories.route("/").get(getCategories);
 
 module.exports = categories;
