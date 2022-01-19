e01dc39a141364582e2540cde9c3ab6ffde82380
Edwin Ortiz <edarcode@gmail.com>
2022-01-19 03:52:26 -0500

    feat(backend/src/routes/products/controllerGet/getProduct.controller.js) filter product by name

--- backend/src/routes/products/controllerGet/getProduct.controller.js
+++ backend/src/routes/products/controllerGet/getProduct.controller.js
@@ -1,14 +1,15 @@
 const { PRODUCT_PER_PAGE } = require("../../../constants/product");
-const { Product } = require("../../../db");
+const { Product, Op } = require("../../../db");
 const { propsFindAndCountAll } = require("./services/basicPropsFind");
 const { includeCategory } = require("./services/includeCategory");
 
 const getProduct = async (req, res, next) => {
   try {
-    const { idCategory, page = 0, typeOrder } = req.query;
+    const { name, idCategory, page = 0, typeOrder } = req.query;
     const propsFind = propsFindAndCountAll(page, typeOrder); //me trae las props basicas para le find
     const include = includeCategory(idCategory);
     const { count, rows } = await Product.findAndCountAll({
+      where: name ? { name: { [Op.iLike]: `%${name}%` } } : {},
       include,
       ...propsFind,
     });
