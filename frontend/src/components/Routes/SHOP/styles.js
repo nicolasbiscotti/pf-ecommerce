7aeb6322a0ee9159ffba6af4cd3a1f3447f70ea8
Leila-Bula <leilabula22@gmail.com>
2022-01-20 13:19:30 -0500

    listado de productos /shop avance para merge 1

--- frontend/src/components/Routes/SHOP/styles.js
+++ frontend/src/components/Routes/SHOP/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ShopStyled=styled.div`
     display: flex;
-    margin-top: 170px;
+    margin-top: 230px;
     background-color: #f7f7f7;
 `;
 
