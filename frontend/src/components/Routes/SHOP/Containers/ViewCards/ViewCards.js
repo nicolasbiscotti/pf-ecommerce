82be18e2bdc5771285df98dadfb5a67a7211f2b5
Leila-Bula <leilabula22@gmail.com>
2022-01-20 18:18:41 -0500

    /shop todo funcional solo falta estilo y filter by

--- frontend/src/components/Routes/SHOP/Containers/ViewCards/ViewCards.js
+++ frontend/src/components/Routes/SHOP/Containers/ViewCards/ViewCards.js
@@ -3,26 +3,6 @@ import { useSelector } from "react-redux";
 import ProductCard from "../../../../common/Productcard/index";
 import { ViewcardStyled } from "./ViewCardStyled";
 
-const Celda=function({array}){
-    return (
-        <tr>
-            {array.map((p,i)=><td><ProductCard product={p} key={i}/></td>)}
-        </tr>
-    )
-}
-const Filas=function({array,nCeldsForRows}){
-    let filas=[];
-    let nRows=Math.ceil(array.length/nCeldsForRows)
-    for(let i=0;i<nRows;i++){
-        filas.push(array.slice(i*nCeldsForRows,(i+1)*nCeldsForRows))
-    }
-    return (
-        <>
-            {filas.map((e)=><Celda array={e}/>)}
-        </>
-    )
-}
-
 export default function Viewcards(){
     const products=useSelector((state)=>state.products.isLoading?[]:state.products.products.products) || []
     console.log(products);
@@ -35,9 +15,7 @@ export default function Viewcards(){
     }else{
         return (
             <ViewcardStyled>
-                <table>
-                    <Filas array={products} nCeldsForRows={2}/>
-                </table>
+                {products.map((p,i)=><ProductCard key={i} product={p}/>)}
             </ViewcardStyled>
         )
     }
