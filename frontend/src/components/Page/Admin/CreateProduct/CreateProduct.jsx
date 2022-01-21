99e91443b4fe9cb68e6e828e9f1679b0fd29c9d1
Edwin Ortiz <edarcode@gmail.com>
2022-01-21 15:01:10 -0500

    refactor(frontend/src/components/Page/Admin/CreateProduct/CreateProduct.jsx)

--- frontend/src/components/Page/Admin/CreateProduct/CreateProduct.jsx
+++ frontend/src/components/Page/Admin/CreateProduct/CreateProduct.jsx
@@ -2,7 +2,29 @@ import React from "react";
 import { CreateProductStyled } from "./style";
 
 const CreateProduct = () => {
-  return <CreateProductStyled>Create Product</CreateProductStyled>;
+  return (
+    <CreateProductStyled>
+      <input type="text" placeholder="name" />
+      <input type="number" placeholder="sale price" />
+      <input type="number" placeholder="purchase price" />
+      <input type="number" placeholder="stock" />
+      <textarea
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+        placeholder="description"
+      ></textarea>
+      <input type="file" />
+      <input type="file" />
+      <select name="" id="">
+        <option value="">suppliers</option>
+      </select>
+      <select name="" id="">
+        <option value="">categories</option>
+      </select>
+    </CreateProductStyled>
+  );
 };
 
 export default CreateProduct;
