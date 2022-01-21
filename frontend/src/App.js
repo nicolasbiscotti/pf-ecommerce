3e60a3937d8086d8c0d656d0ead91669fc312fc4
Edwin Ortiz <edarcode@gmail.com>
2022-01-21 00:46:31 -0500

    hotfix(frontend/src/App.js)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import { Footer } from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Routes/Home";
 import ProductDetail from "./components/ProductDetail";
+import Admin from "./components/Page/Admin/Admin";
+import CreateProduct from "./components/Page/Admin/CreateProduct/CreateProduct";
 
 function App() {
   return (
@@ -14,11 +16,11 @@ function App() {
       <Routes>
         <Route path="favorites" />
         <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop/>}/>
+        <Route path="/shop" element={<Shop />} />
         <Route path="/favorites" />
-        <Route path={`/detailts/:idProduct`} element={<ProductDetail/>}/>
-        <Route path="dashboard/*">
-          <Route path="addproducts" />
+        <Route path={`/detailts/:idProduct`} element={<ProductDetail />} />
+        <Route path="admin/*" element={<Admin />}>
+          <Route path="create/product" element={<CreateProduct />} />
         </Route>
       </Routes>
       <Footer />
