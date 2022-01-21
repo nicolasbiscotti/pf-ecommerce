d2e0887c89600cf8cd874187d8ae8af68c22ddbe
Leila-Bula <leilabula22@gmail.com>
2022-01-20 22:53:53 -0500

    estilo de categories

--- frontend/src/components/Routes/SHOP/Comp/Category/CategoryStyle.js
+++ frontend/src/components/Routes/SHOP/Comp/Category/CategoryStyle.js
@@ -1,5 +1,36 @@
 import styled from "styled-components";
 
 export const CategoriesStyled=styled.div`
-    background-color: white;
+    background-color: #fff;
+    border-radius: 3px;
+    margin-top: 10px;
+    margin-left: 10px;
+    border-bottom: solid #fff 10px;
+    font-family: "Rubik",sans-serif;
+
+    h1,h3{
+        margin-top: 10px;
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    h1{
+        padding: 3%;
+        padding-top: 4%;
+        border-bottom: solid #0dcaf0 2px;
+        color: #222;
+    }
+
+    h3{
+        padding-left: 3%;
+        color: #666;
+        border-bottom: solid #F3FCFC  1px;
+    }
+
+    @media screen and (min-width: 550) and (max-width: 768) {
+        h1{
+            width: 80%;
+            font-size: 20%;
+        }
+    }
 `;
--- frontend/src/components/Routes/SHOP/Containers/Filterbar/FilterbarStyled.js
+++ frontend/src/components/Routes/SHOP/Containers/Filterbar/FilterbarStyled.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
 export const FilterbarStyled=styled.div`
-    width: 28%
+    width: 28%;
+
+    @media screen and (min-width: 550px) and (max-width: 768px){
+        width: 15%
+    }
+
+    @media screen and (min-width: 0px) and (max-width: 550px){
+        display:none;
+    }
 `;
