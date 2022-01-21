d9164678be7a359124d8d1b2d2a2e9920d83857c
FerrnA <arriondovfernando@gmail.com>
2022-01-21 07:28:22 -0300

    feat(frontend/src/components/Header): para componente de búsqueda

--- frontend/src/components/Header/NavMain/Search/Search.jsx
+++ frontend/src/components/Header/NavMain/Search/Search.jsx
@@ -1,14 +1,72 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { SearchStyled } from "./SearchStyled";
 import searchIcon from "../../../../utilsStyles/utilsImages/search-iconwhite.png";
 
-function Search() {
+function Search({ data }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [inputWord, setInputWord] = useState("");
+  const searchInput = useRef();
+
+  const handleCleanInput = (f = [], i = "") => {
+    setFilteredData(f);
+    setInputWord(i);
+  };
+  const handleFilter = (event) => {
+    if (event.target.value === "") {
+      handleCleanInput();
+    } else {
+      const searchWord = event.target.value;
+      let count = 0; //hardcodeo para cortar a 15 elementos filtrados
+      const newFilter = data.filter((value) => {
+        if (count < 16) {
+          const isInclude = value.name
+            .toLowerCase()
+            .includes(searchWord.toLowerCase());
+          if (isInclude) count++;
+          return isInclude;
+        } else return false;
+      });
+      handleCleanInput(newFilter, searchWord);
+    }
+  };
+  const handleSugestionClick = (event) => {
+    handleCleanInput([], event.target.innerHTML);
+    searchInput.current.focus();
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit"); /// HANDLE SUBMIT
+    handleCleanInput();
+  };
+
   return (
     <SearchStyled>
-      <input placeholder="Search our catalog" />
-      <button>
+      <input
+        type="text"
+        placeholder="Search our catalog"
+        onChange={handleFilter}
+        value={inputWord}
+        ref={searchInput}
+        onKeyDown={(event) =>
+          event.key === "Enter" ? handleOnSubmit(event) : console.log()
+        }
+      />
+      <button onClick={handleOnSubmit}>
         <img src={searchIcon} alt="search icon" />
       </button>
+
+      {filteredData.length !== 0 && (
+        <div className="dataResult">
+          {filteredData.slice(0, 4).map((value, key) => {
+            return (
+              <p key={key} onClick={(event) => handleSugestionClick(event)}>
+                {value.name}
+              </p>
+            );
+          })}
+        </div>
+      )}
     </SearchStyled>
   );
 }
--- frontend/src/components/Header/NavMain/Search/SearchStyled.js
+++ frontend/src/components/Header/NavMain/Search/SearchStyled.js
@@ -5,14 +5,21 @@ import { hoverPointer } from "../../../../utilsStyles/utilsMixins";
 export const SearchStyled = styled.div`
   display: flex;
   align-items: center;
+  position: relative;
   input {
     width: 40vw;
     height: 5rem;
     padding: 1.3rem 1.2rem 1.1rem;
     border: none;
     border-radius: 0.5rem 0 0 0.5rem;
+    color: #0c1417;
     &::placeholder {
       color: #212529;
+      font-size: 1.4rem;
+    }
+    font-size: 1.6rem;
+    &:focus {
+      outline: none;
     }
   }
   button {
@@ -26,6 +33,26 @@ export const SearchStyled = styled.div`
       width: 1.4rem;
       height: 1.4rem;
       margin: 0 auto;
+    }
+  }
+  .dataResult {
+    position: absolute;
+    top: 5.1rem;
+    left: 0;
+    width: 100%;
+    max-height: 13.5rem;
+    z-index: 2;
+    background-color: white;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    display: flex;
+    flex-direction: column;
+    p {
+      height: 4.5rem;
+      margin-left: 1rem;
+      display: flex;
+      align-items: center;
+      color: #0c1417;
+      ${hoverPointer};
     }
   }
 `;
