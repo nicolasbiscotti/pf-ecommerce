e39f35fe1714d7ee90aba97cf937ce0255bfb0a4
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-01-27 12:21:30 -0300

    refactor(frontend/src/components/Header/NavMain/UserItems/Box/Box.jsx): link to login page

--- frontend/src/components/Header/NavMain/UserItems/Box/Box.jsx
+++ frontend/src/components/Header/NavMain/UserItems/Box/Box.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BoxStyled } from "./BoxStyled";
 import userIcon from "../../../../../utilsStyles/utilsImages/loggin-icon.png";
 import cartIcon from "../../../../../utilsStyles/utilsImages/cart-icon.png";
+import { Link } from "react-router-dom";
 
 function Box({ Imgsrc, Imgalt, Text }) {
   const userIc = userIcon;
@@ -11,8 +12,12 @@ function Box({ Imgsrc, Imgalt, Text }) {
     <BoxStyled>
       <img src={Imgsrc === "user" ? userIc : cartIc} alt={Imgalt} />
       <div>
-        <p>{Text[0]}</p>
-        <p>{Text[1].includes(".") ? <b>{Text[1]}</b> : Text[1]}</p>
+        <Link to="/login">
+          <p>{Text[0]}</p>
+        </Link>
+        <Link to="/login/register">
+          <p>{Text[1].includes(".") ? <b>{Text[1]}</b> : Text[1]}</p>
+        </Link>
       </div>
     </BoxStyled>
   );
