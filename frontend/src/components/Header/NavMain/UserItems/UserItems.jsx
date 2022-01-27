47991875112a2d3d4678b30e8d5b52dfa8e1ebf0
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-01-27 15:55:55 -0300

    fix: vercel erros

--- frontend/src/components/Header/NavMain/UserItems/UserItems.jsx
+++ frontend/src/components/Header/NavMain/UserItems/UserItems.jsx
@@ -48,6 +48,7 @@ export default function UserItems() {
       setUser({});
     } catch (error) {
       setFetchError(error.message);
+      console.log(fetchError);
     }
   };
 
--- frontend/src/components/Login/LoginPage/LoginPage.jsx
+++ frontend/src/components/Login/LoginPage/LoginPage.jsx
@@ -1,11 +1,10 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { Footer } from "../../Footer/Footer";
 import { StyledLoginPage } from "./styled";
 
 export default function LoginPage() {
   const message = useSelector((state) => state.message.message);
-  const dispatch = useDispatch();
   return (
     <StyledLoginPage>
       <header></header>
