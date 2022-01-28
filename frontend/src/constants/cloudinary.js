5888305529121e7b783d22315674ff1fe3720a29
luiggylml <luiggylml@gmail.com>
2022-01-27 23:13:55 -0500

    refactor(*)

--- frontend/src/constants/cloudinary.js
+++ frontend/src/constants/cloudinary.js
@@ -1,4 +1,3 @@
-export const cloudName = "dvmsllkkg";
+export const cloudName = process.env.REACT_APP_CLOUDNAME;
 export const urlCloudinary = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
-export const cloudNameCat = "dhtyfvv9i";
-export const urlCloudinaryCat = `https://api.cloudinary.com/v1_1/${cloudNameCat}/image/upload`;
+export const namePreset = process.env.REACT_APP_NAME_PRESET;
