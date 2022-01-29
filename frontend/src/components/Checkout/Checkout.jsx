9cb51e4a5d988e54e916293f84432dab6f3b1831
FerrnA <arriondovfernando@gmail.com>
2022-01-28 21:19:28 -0300

    asd

--- frontend/src/components/Checkout/Checkout.jsx
+++ frontend/src/components/Checkout/Checkout.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import PayPalCheckout from "./PayPalCheckout/PayPalCheckout";
-import MPCheckout from "./MPCheckout/MPCheckout";
+//import MPCheckout from "./MPCheckout/MPCheckout";
 import { CheckoutStyled } from "./CheckoutStyled";
 
 function Checkout() {
   return (
     <CheckoutStyled>
       <PayPalCheckout />
-      <MPCheckout />
+      {/* <MPCheckout /> */}
     </CheckoutStyled>
   );
 }
