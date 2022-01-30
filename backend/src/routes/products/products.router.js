a924dfb4dda6003072c3f7d24c2843cc6a469f31
Edwin Ortiz <edarcode@gmail.com>
2022-01-30 16:27:29 -0500

    feat(backend/src/routes/products/controllerGet/productByidAdmin.controller.js) get detail product for admin

--- backend/src/routes/products/products.router.js
+++ backend/src/routes/products/products.router.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const getProduct = require("./controllerGet/getProduct.controller");
 const getProductAdmin = require("./controllerGet/getProductAdmin.controller");
 const productById = require("./controllerGet/productById.controller");
+const getProductByIdAdmin = require("./controllerGet/productByidAdmin.controller");
 const createProduct = require("./controllerPost/createProduct.controller");
 const updateProduct = require("./controllerUpdate/updateProduct.controller");
 
 const products = Router();
 
+products.route("/admin/:id").get(getProductByIdAdmin);
 products.route("/admin").get(getProductAdmin);
 products.route("/id/:id").get(productById);
 products.route("/").get(getProduct);
