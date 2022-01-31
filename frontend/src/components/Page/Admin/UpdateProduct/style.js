86cefd774c82b333467ad14c90526087aff8e005
Edwin Ortiz <edarcode@gmail.com>
2022-01-31 02:27:02 -0500

    feat(frontend/src/components/Page/Admin/UpdateProduct/style.js) styles

--- frontend/src/components/Page/Admin/UpdateProduct/style.js
+++ frontend/src/components/Page/Admin/UpdateProduct/style.js
@@ -1,8 +1,44 @@
 import styled from "styled-components";
+import { bgFormAdmin } from "../../../../utilsStyles/utilsColors";
 
 export const UpdateProductStyled = styled.div`
-  color: white;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
   @media (min-width: 768px) {
     margin-left: 250px;
+  }
+  form {
+    display: grid;
+    gap: 3px;
+    color: white;
+    min-width: 320px;
+    max-width: 640px;
+    padding: 25px;
+    background-color: ${bgFormAdmin};
+    .selects {
+      display: grid;
+      gap: 3px;
+    }
+    .files {
+      display: grid;
+      gap: 3px;
+    }
+    @media (min-width: 375px) {
+      .files {
+        grid-template-columns: auto auto 1fr;
+      }
+      .selects {
+        grid-template-columns: auto auto 1fr;
+      }
+    }
+    @media (min-width: 768px) {
+      .price {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 3px;
+      }
+    }
   }
 `;
