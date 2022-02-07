507b083f7008cd5e4b19fa9fded68aab923fd848
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-02-06 21:09:24 -0300

    fix: google login

--- backend/src/routes/users/users.router.js
+++ backend/src/routes/users/users.router.js
@@ -3,9 +3,11 @@ const passport = require("passport");
 const listUsers = require("./controllerGet/listUsers.controller");
 const userById = require("./controllerGet/userById.controller");
 const userByOauthProfile = require("./controllerGet/userByOauthProfile.controller");
+const verification = require("./controllerGet/verification.controller");
 const createSocialUser = require("./controllerPost/createSocialUser.controller");
 const createUser = require("./controllerPost/createUser.controller");
 const updateUserController = require("./controllerPut/updateUser.controller");
+const userService = require("./services/userService");
 
 const users = Router();
 
@@ -21,6 +23,7 @@ users
   );
 users.route("/social").post(passport.authenticate("jwt"), createSocialUser);
 users.route("/social").get(passport.authenticate("admin"), userByOauthProfile);
+users.route("/verify/:userId/:token").get(verification);
 users
   .route("/")
   .get(passport.authenticate("jwt"), passport.authenticate("admin"), listUsers);
