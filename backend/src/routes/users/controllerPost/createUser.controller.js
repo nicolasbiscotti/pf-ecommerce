1b889fa7fca9cb5729011cb8d08d25572ff316a0
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-02-07 09:50:41 -0300

    feat(backend/src/config/nodemailer.js): send verification email

--- backend/src/routes/users/controllerPost/createUser.controller.js
+++ backend/src/routes/users/controllerPost/createUser.controller.js
@@ -1,9 +1,18 @@
+const transporter = require("../../../config/nodemailer");
+const { URL_BASE_BACKEND } = require("../../../constants/config");
 const userService = require("../services/userService");
 
 const createUser = async (req, res, next) => {
   try {
     const [user, created] = await userService.createUser(req.body);
     if (created) {
+      transporter.sendMail({
+        from: '"verify email 👻" <testedarcode@gmail.com>', // sender address
+        to: user.email, // list of receivers
+        subject: "verify email ✔", // Subject line
+        text: `${URL_BASE_BACKEND}/users/verify/${user.id}/${user.verificationToken}`, // plain text body
+        //html: "<b>Hello world?</b>", // html body
+      });
       res.json({
         successfully: true,
         message: {
